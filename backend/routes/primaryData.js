4c63e059a6c5ff95639448c2aff34a6222054644
bryantn98 <81392255+bryantn98@users.noreply.github.com>
2022-10-03 18:22:42 -0500

    Update primaryData.js

--- backend/routes/primaryData.js
+++ backend/routes/primaryData.js
@@ -92,4 +92,14 @@ router.put("/:id", (req, res, next) => {
     );
 });
 
+//DELETE attendee by id
+router.delete('/:id', function (req, res) {
+    primarydata.findByIdAndRemove({_id: req.params.id})
+    .exec()
+    .then(doc => {
+        if (!doc) {return res.status(404).send({error: "ID not found"});}
+        return res.send("ID successfully removed");
+    })
+})
+
 module.exports = router;
